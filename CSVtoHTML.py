bdb77f79a4f67fd6579b4f21d6e9a3179870f7eb
Lord Mico <michael.costas377@gmail.com>
2020-06-15 16:17:37 -0400

    Change output file name - Update the output file name to something more clear - Get title parameters - TODO: Check title params

--- CSVtoHTML.py
+++ CSVtoHTML.py
@@ -28,28 +28,45 @@ for currentCsv in csvFiles:
                     eventsByWeek[nearestMonday].append(row[0])
                 else:
                     eventsByWeek[nearestMonday] = [row[0]]
-            #print(', '.join(row))
             else:
                 firstRow = False
-        #TODO: Create new file
-        outFile = open("calendar.html", "w")
+        
+        sortedEventsByWeek = sorted(eventsByWeek.items())
+
+        if ".CSV" in currentCsv:
+            fileName = currentCsv.replace('.CSV','.html')
+        elif ".csv" in currentCsv:
+            fileName = currentCsv.replace('.csv','.html')
+        else:
+            filename = currentCsv + ".html"
+
+        outFile = open(fileName, "w")
         outFile.write("<!DOCTYPE html>\n<html>\n<head>\n</head>\n<body>")
         
-        title = "Tentative schedule for 1B SYDE classes for Spring 2020."
+        responses = input("Enter the Term (1A to 4B), class (SYDE/BME), term (Spring, Summer, Fall) and year separated by commas: ")
+
+        acceptableResponse = True
+        responseList = responses.split(",")
+        for response in responseList:
+            response = response.strip()
+
+        # acceptableResponse = True if responseList[0][0]
+
+        if not acceptableResponse:
+            responses = input("Enter the Term (1A to 4B), class (SYDE/BME), term (Spring, Summer, Fall) and year separated by commas: ")
+
+        
+        title = "Tentative schedule for " + responseList[0] + " " + responseList[1] + " classes for " + responseList[2] + " " + responseList[3] + "."
         warning = "To be used for PLANNING PURPOSES ONLY. Confirm due dates with the course instructors."
 
         outFile.write("<h2><span style=\"font-family: tahoma, sans-serif;\"><strong>" + title + "<br /><br /><span style=\"color: #9900ff;\">" + warning + "</span><br /><br /></strong></span></h2>")
         outFile.write("<table style=\"width: 946px;\" height=\"1004\">\n<tbody>")
 
-        for key, value in eventsByWeek.items():
-            outFile.write("<tr style=\"width: 233.542px;\">\n<td><span style=\"font-family: tahoma, sans-serif;\"><strong> Week of " + key.strftime("%Y/%m/%d") + " &mdash; <br />Deliverables &amp;&nbsp;Weights </strong></span></td>")
-            outFile.write("<td style=\"width: 697.708px;\"> <span style=\"font-family: tahoma, sans-serif;\">" + "<br />".join(value) + "</span></td>")
+        for currentItem in sortedEventsByWeek:
+            outFile.write("<tr style=\"width: 233.542px;\">\n<td><span style=\"font-family: tahoma, sans-serif;\"><strong> Week of " + currentItem[0].strftime("%B %d") + " &mdash; <br />Deliverables &amp;&nbsp;Weights </strong></span></td>")
+            outFile.write("<td style=\"width: 697.708px;\"> <span style=\"font-family: tahoma, sans-serif;\">" + "<br />".join(currentItem[1]) + "</span></td>")
             outFile.write("</tr>")
 
         outFile.write("</tbody>\n</table>\n<pre><br /><br /></pre>\n</body>\n</html>")
         
         outFile.close()
-
-    # outputFile = currentCsv.replace('.CSV','')
-    # outputFile = outputFile.replace('.csv','')
-    # outputFileDir = 'output/' + outputFile + '.ics'
